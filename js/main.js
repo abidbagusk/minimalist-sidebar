9de8145d760bc0b36f55fd50c129f8bc19968cb5
abidbagusk <abidbagudkurniawan@gmail.com>
2019-01-26 18:43:56 +0700

    add animation to submenu-one

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+    $('.wrapper-submenu-one').click(function (e) {
+        $('.sub-menu-one').slideToggle(500, function () {
+            $('.sub-menu-two').slideUp(400, function () {
+                $('.sub-menu-three').slideUp(400);
+            });
+        });
+        e.preventDefault();
+    });
     $('.wrapper-submenu-two').click(function (e) {
         $('.sub-menu-one').slideUp(400, function () {
             $('.sub-menu-two').slideToggle(500, function () {
