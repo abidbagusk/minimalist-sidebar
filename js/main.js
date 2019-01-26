48c7fa00145193ceaccaa645fa09f24aa5476209
abidbagusk <abidbagudkurniawan@gmail.com>
2019-01-26 19:08:27 +0700

    create function to call sidenav when user click on hamburger menu

--- js/main.js
+++ js/main.js
@@ -26,3 +26,11 @@ $(document).ready(function () {
     });
 
 });
+
+function openNav() {
+    document.getElementById('mySidenav').style.width = '250px';
+}
+
+function closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+}
