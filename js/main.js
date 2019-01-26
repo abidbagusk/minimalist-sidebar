90694c46fda475e2d99d3557288718d14dfeafb6
abidbagusk <abidbagudkurniawan@gmail.com>
2019-01-26 18:38:38 +0700

    fix animation each other. if submenu-two and submenu-three are open and user click on the submenu two, then submenu-three will hide.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function () {
     $('.wrapper-submenu-two').click(function (e) {
-        $('.sub-menu-one').slideToggle(200, function () {
-            $('.sub-menu-two').slideToggle(500);
+        $('.sub-menu-one').slideUp(400, function () {
+            $('.sub-menu-two').slideToggle(500, function () {
+                $('.sub-menu-three').slideUp(400);
+            });
         });
         e.preventDefault();
     });
+
+    $('.wrapper-submenu-three').click(function (e) {
+        $('.sub-menu-one').slideUp(400, function () {
+            $('.sub-menu-three').slideToggle(500, function () {
+                $('.sub-menu-two').slideUp(400);
+            });
+        });
+        e.preventDefault();
+    });
+
 });
